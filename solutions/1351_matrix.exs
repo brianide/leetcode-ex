c3f90bd1b1ad918dd376844795d6dde742ca67e9
brianide <brianide@gmail.com>
2023-06-08 13:45:05 -0500

    Simplified 1351

--- solutions/1351_matrix.exs
+++ solutions/1351_matrix.exs
@@ -2,12 +2,11 @@ defmodule Solution do
   @spec count_negatives(grid :: [[integer]]) :: integer
   def count_negatives(grid) do
     width = length(hd(grid))
-    Enum.reduce(grid, {0, width}, fn line, {count, maxlen} ->
-      case Enum.take(line, maxlen) |> Enum.find_index(&(&1 < 0)) do
-        nil -> {count + width - maxlen, maxlen}
-        n -> {count + width - n, n}
+    Enum.reduce(grid, 0, fn line, count ->
+      case Enum.find_index(line, &(&1 < 0)) do
+        nil -> count
+        n -> count + width - n
       end
     end)
-    |> elem(0)
   end
 end
