33b51ef5c30ca0162592e4b165c7eefa120fe746
brianide <brianide@gmail.com>
2023-11-20 13:51:40 -0600

    Simplified solution for 2391

--- solutions/2391_garbage.exs
+++ solutions/2391_garbage.exs
@@ -1,22 +1,13 @@
 defmodule Solution do
   @spec garbage_collection(garbage :: [String.t], travel :: [integer]) :: integer
   def garbage_collection(garbage, travel) do
-    {trash, indices} =
-      Enum.with_index(garbage)
-      |> Enum.reduce({0, %{}}, fn {house, index}, {total, indices} ->
-        total = total + String.length(house)
-        indices =
-          String.graphemes(house)
-          |> Enum.uniq()
-          |> Enum.reduce(indices, &Map.put(&2, &1, index))
-        {total, indices}
-      end)
-
-    travel =
-      Map.values(indices)
-      |> Enum.flat_map(&Enum.take(travel, &1))
-      |> Enum.sum()
-
-    trash + travel
+    Enum.zip(garbage, [0 | travel])
+    |> Enum.reverse()
+    |> Enum.reduce({0, MapSet.new()}, fn {trash, travel}, {total, seen} ->
+      seen = String.graphemes(trash) |> Enum.reduce(seen, &MapSet.put(&2, &1))
+      total = total + String.length(trash) + MapSet.size(seen) * travel
+      {total, seen}
+    end)
+    |> then(&Kernel.elem(&1, 0))
   end
 end
